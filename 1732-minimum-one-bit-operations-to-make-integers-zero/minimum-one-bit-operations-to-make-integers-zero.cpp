d92d6d0ffd765e7774612cba383a8b1dd7bb5d02
Aamodit <81962193+Aamodit@users.noreply.github.com>
2025-12-13 01:59:31 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (45.02%) - LeetSync

--- 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cpp
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     int minimumOneBitOperations(int n) {
-        if (n == 0) return 0;
-        int k = 31 - __builtin_clz(n);
-        return (1 << (k + 1)) - 1 - minimumOneBitOperations(n ^ (1 << k));
+        int res = 0;
+        while (n > 0) {
+            res ^= n;
+            n >>= 1;
+        }
+        return res;
     }
 };
