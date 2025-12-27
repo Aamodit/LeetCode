0ebc89ba3410c7051027df7b06c881968bc903dc
Aamodit <81962193+Aamodit@users.noreply.github.com>
2025-12-27 11:35:51 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (18.02%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        vector<int> ans;
         int n = nums.size();
-        int k = 0;
-        for (int i = 0; i < n; i++) {
-            if (!binary_search(ans.begin(), ans.end(), nums[i])) {
-                ans.push_back(nums[i]);
+        if (n == 0) {
+            return 0;
+        }
+        int k = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
                 k++;
             }
         }
 
-        for (int i = 0; i < k; i++) {
-            nums[i] = ans[i];
-        }
         return k;
     }
 };
