cd48d0c612e660ad2a1827da5259d10b12a2ef78
Aamodit <81962193+Aamodit@users.noreply.github.com>
2026-01-01 16:47:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (93.34%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string ans;
+        int l = 0, r = s.size() - 1;
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] >= 'A' && s[i] <= 'Z') {
-                ans.push_back(s[i] + 32);
-            } else if ((s[i] >= 'a' && s[i] <= 'z') ||
-                       (s[i] >= '0' && s[i] <= '9')) {
-                ans.push_back(s[i]);
-            }
-        }
+        while (l < r) {
+            while (l < r && !isalnum(s[l])) l++;
+            while (l < r && !isalnum(s[r])) r--;
 
-        int m = ans.length();
-        for (int i = 0; i < m / 2; i++) {
-            if (ans[i] != ans[m - 1 - i])
+            if (tolower(s[l]) != tolower(s[r]))
                 return false;
+
+            l++;
+            r--;
         }
         return true;
     }
