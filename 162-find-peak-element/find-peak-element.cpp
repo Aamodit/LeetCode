f256f02d0e76edde6060634954c8eb9fd96552c5
Aamodit <81962193+Aamodit@users.noreply.github.com>
2026-01-05 16:56:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (82.32%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int n = nums.size(), m = INT_MIN, l = 0;
-        for (int i = 0; i < n; i++) {
-            if (m < nums[i]) {
-                m = nums[i];
-                l = i;
+        int l = 0, r = nums.size() - 1;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] < nums[mid + 1]) {
+                // increasing slope → peak on right
+                l = mid + 1;
+            } else {
+                // decreasing slope → peak at mid or left
+                r = mid;
             }
         }
-        return l;
+
+        return l;  // l == r is guaranteed to be a peak
     }
 };
