eaddb6015a0eb4eeceb26ab8fb4ecce9ff19faae
Aamodit <81962193+Aamodit@users.noreply.github.com>
2026-03-12 00:51:56 +0530

    Time: 2 ms (50.78%) | Memory: 8.6 MB (37.60%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
-    bool isPalindrome(int x) {
-        string y = to_string(x);
-        int n = y.size();
-        for (int i = 0; i < n / 2; i++) {
-            if (y[i] != y[n - 1 - i]) {
-                return false;
-            }
+    int rev(int n) {
+        int ans = 0;
+        while (n > 0) {
+            if (ans >= INT_MAX / 10 || ans <= INT_MIN / 10)
+                return 0;
+            ans = (ans * 10) + (n % 10);
+            n = n / 10;
         }
-        return true;
+        return ans;
+    }
+
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        if (rev(x) == x) {
+            return true;
+        }
+        return false;
     }
 };
