602c46fcdb140524c6ebd9dd5f3a018146627769
Aamodit <81962193+Aamodit@users.noreply.github.com>
2026-03-18 10:27:05 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (44.56%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& m, int t) {
+        for(int i=0;i<m.size();i++)
+        {
+            for(int j=0;j<m[i].size();j++)
+            {
+                if(m[i][j]==t)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
