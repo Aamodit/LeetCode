48c6976f785f19d430ba7297c8edf9daf0ba2dc1
Aamodit <81962193+Aamodit@users.noreply.github.com>
2026-03-19 23:40:49 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (78.19%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,36 +1,20 @@
 class Solution {
 public:
-    int findR(vector<vector<int>>& m, int t) {
-        int l = 0, r = m.size() - 1, mid;
-        while (l <= r) {
-            mid = l + (r - l) / 2;
-            if (t >= m[mid][0] && t <= m[mid][m[mid].size() - 1]) {
-                return mid;
-            } else if (t < m[mid][m[mid].size() - 1]) {
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
-        return -1;
-    }
-
     bool searchMatrix(vector<vector<int>>& m, int t) {
+        int tr = m.size();
+        int tc = m[0].size();
 
-        int row = findR(m, t);
-
-        if (row == -1) {
-            return false;
-        }
-        int l = 0, r = m[row].size() - 1, mid;
+        int l = 0, r = tc * tr - 1;
         while (l <= r) {
-            mid = l + (r - l) / 2;
-            if (m[row][mid] == t) {
+            int mid = l + (r - l) / 2;
+            int cr = mid / tc;
+            int cc = mid % tc;
+            if (t == m[cr][cc]) {
                 return true;
-            } else if (m[row][mid] > t) {
-                r = mid - 1;
-            } else {
+            } else if (t > m[cr][cc]) {
                 l = mid + 1;
+            } else {
+                r = mid - 1;
             }
         }
         return false;
