b1a35c68fc5f325abce5629b87dd6b7f635c5773
Aamodit <81962193+Aamodit@users.noreply.github.com>
2026-03-29 16:12:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (14.62%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& mat) {
-        vector<int> ans;
-
         int rs = 0, re = mat.size() - 1;
         int cs = 0, ce = mat[0].size() - 1;
+        vector<int> ans;
         while (rs <= re && cs <= ce) {
-            if (rs <= re)
-                for (int i = cs; i <= ce; i++) {
-                    ans.push_back(mat[rs][i]);
-                }
+            for (int i = cs; i <= ce; i++) {
+                ans.push_back(mat[rs][i]);
+            }
             rs++;
-            if (cs <= ce)
-                for (int i = rs; i <= re; i++) {
-                    ans.push_back(mat[i][ce]);
-                }
+            for (int i = rs; i <= re; i++) {
+                ans.push_back(mat[i][ce]);
+            }
             ce--;
-            if (rs <= re)
+            if (rs <= re) {
                 for (int i = ce; i >= cs; i--) {
                     ans.push_back(mat[re][i]);
                 }
+            }
             re--;
-
-            if (cs <= ce)
+            if (cs <= ce) {
                 for (int i = re; i >= rs; i--) {
                     ans.push_back(mat[i][cs]);
                 }
+            }
             cs++;
         }
         return ans;
