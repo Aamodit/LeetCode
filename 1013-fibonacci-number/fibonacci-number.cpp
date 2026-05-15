3481f149a16713d21381010e6d6ef3445529eeeb
Aamodit <81962193+Aamodit@users.noreply.github.com>
2026-05-15 15:58:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (65.14%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int a = 0, b = 1, c;
+        while (n >= 2) {
+            c = a + b;
+            a = b;
+            b = c;
+            n--;
+        }
+
+        return c;
+    }
+};
