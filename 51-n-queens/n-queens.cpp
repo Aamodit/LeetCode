83db5ef40778da382fcb833bfd8da3adea390f61
Aamodit <81962193+Aamodit@users.noreply.github.com>
2026-05-24 15:35:54 +0530

    Time: 3 ms (60.82%) | Memory: 10.4 MB (47.25%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,56 +1,63 @@
 class Solution {
 public:
     vector<vector<string>> ans;
-    vector<string> b;
 
-    bool isSafe(int r, int c, int n) {
-        // check vertical & horizontal
-        for (int i = 0; i < n; i++) {
-            if (b[r][i] == 'Q' || b[i][c] == 'Q') {
-                return false;
-            }
-            if (r - i >= 0 && c - i >= 0) {
-                if (b[r - i][c - i] == 'Q') {
-                    return false;
-                }
-            }
-            if (r + i < n && c + i < n) {
-                if (b[r + i][c + i] == 'Q') {
-                    return false;
-                }
-            }
-            if (r - i >= 0 && c + i < n) {
-                if (b[r - i][c + i] == 'Q') {
-                    return false;
-                }
-            }
-            if (r + i < n && c - i >= 0) {
-                if (b[r + i][c - i] == 'Q') {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+    vector<string> board;
 
-    void sQ(int r, int n) {
+    vector<bool> col;
+    vector<bool> urd; // r+c
+    vector<bool> uld; // r-c+n-1
+
+    void solve(int r, int n) {
+
+        // all queens placed
         if (r == n) {
-            ans.push_back(b);
+            ans.push_back(board);
             return;
         }
 
         for (int c = 0; c < n; c++) {
-            if (isSafe(r, c, n)) {
-                b[r][c] = 'Q';
-                sQ(r + 1, n);
-                b[r][c] = '.';
+
+            // unsafe position
+            if (col[c] || urd[r + c] || uld[r - c + n - 1]) {
+                continue;
             }
+
+            // place queen
+            board[r][c] = 'Q';
+
+            // mark attacks
+            col[c] = true;
+            urd[r + c] = true;
+            uld[r - c + n - 1] = true;
+
+            // next row
+            solve(r + 1, n);
+
+            // backtrack
+            board[r][c] = '.';
+
+            // unmark attacks
+            col[c] = false;
+            urd[r + c] = false;
+            uld[r - c + n - 1] = false;
         }
     }
+
     vector<vector<string>> solveNQueens(int n) {
-        // initialize board with .
-        b = vector<string>(n, string(n, '.'));
-        sQ(0, n);
+
+        // initialize empty board
+        board = vector<string>(n, string(n, '.'));
+
+        // initialize attack trackers
+        col = vector<bool>(n, false);
+
+        urd = vector<bool>(2 * n - 1, false);
+
+        uld = vector<bool>(2 * n - 1, false);
+
+        solve(0, n);
+
         return ans;
     }
 };
