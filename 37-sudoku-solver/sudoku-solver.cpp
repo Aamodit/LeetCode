84b44114054e067ace7af19ff9d35460a06cc74b
Aamodit <81962193+Aamodit@users.noreply.github.com>
2026-05-26 16:11:39 +0530

    Time: 131 ms (85.81%) | Memory: 8.8 MB (70.34%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,52 +1,62 @@
 class Solution {
 public:
     vector<vector<char>> b;
-
-    bool isSafe(int r, int c, char d) {
-        for (int i = 0; i < 9; i++) {
-            if (b[r][i] == d || b[i][c] == d) {
-                return false;
-            }
+    vector<vector<bool>> rb;
+    vector<vector<bool>> cb;
+    vector<vector<bool>> bb;
+    void bt(int r, int c) {
+        if (r == 9) {
+            return;
         }
-
-        int sr = (r / 3) * 3, sc = (c / 3) * 3;
-        for (int i = sr; i <= sr + 2; i++) {
-            for (int j = sc; j <= sc + 2; j++) {
-                if (b[i][j] == d) {
-                    return false;
-                }
-            }
+        if (c == 9) {
+            return bt(r + 1, 0);
         }
-        return true;
+        if (b[r][c] == '.') {
+            return bt(r, c + 1);
+        }
+        int d = b[r][c] - '0';
+        rb[r][d] = true;
+        cb[c][d] = true;
+        bb[(r / 3) * 3 + (c / 3)][d] = true;
+        return bt(r, c + 1);
     }
 
     bool ss(int r, int c) {
         if (r == 9) {
             return true;
         }
-
         if (c == 9) {
             return ss(r + 1, 0);
         }
-
         if (b[r][c] != '.') {
             return ss(r, c + 1);
         }
-
         for (char i = '1'; i <= '9'; i++) {
-            if (isSafe(r, c, i)) {
-                b[r][c] = i;
-                if (ss(r, c + 1)) {
-                    return true;
-                }
-                b[r][c] = '.';
+            int d = i - '0';
+            if (rb[r][d] || cb[c][d] || bb[(r / 3) * 3 + (c / 3)][d]) {
+                continue;
             }
+            b[r][c] = i;
+            rb[r][d] = true;
+            cb[c][d] = true;
+            bb[(r / 3) * 3 + (c / 3)][d] = true;
+            if (ss(r, c + 1)) {
+                return true;
+            }
+            b[r][c] = '.';
+            rb[r][d] = false;
+            cb[c][d] = false;
+            bb[(r / 3) * 3 + (c / 3)][d] = false;
         }
         return false;
     }
 
     void solveSudoku(vector<vector<char>>& board) {
         b = board;
+        rb = vector<vector<bool>>(9, vector<bool>(10, false));
+        cb = vector<vector<bool>>(9, vector<bool>(10, false));
+        bb = vector<vector<bool>>(9, vector<bool>(10, false));
+        bt(0, 0);
         ss(0, 0);
         board = b;
     }
