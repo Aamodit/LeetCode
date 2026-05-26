9e5a8d84f381ac7291d2585cde66fcfa0eff951b
Aamodit <81962193+Aamodit@users.noreply.github.com>
2026-05-26 16:15:33 +0530

    Time: 130 ms (86.43%) | Memory: 8.9 MB (9.93%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,63 +1,103 @@
 class Solution {
 public:
+
     vector<vector<char>> b;
+
+    // row[r][d]
+    // is digit d present in row r?
     vector<vector<bool>> rb;
+
+    // col[c][d]
+    // is digit d present in column c?
     vector<vector<bool>> cb;
+
+    // box[b][d]
+    // is digit d present in box b?
     vector<vector<bool>> bb;
-    void bt(int r, int c) {
-        if (r == 9) {
-            return;
+
+    // preprocessing
+    void preprocess() {
+
+        for(int r = 0; r < 9; r++) {
+
+            for(int c = 0; c < 9; c++) {
+
+                if(b[r][c] == '.')
+                    continue;
+
+                int d = b[r][c] - '0';
+
+                int box = (r/3)*3 + (c/3);
+
+                rb[r][d] = true;
+                cb[c][d] = true;
+                bb[box][d] = true;
+            }
         }
-        if (c == 9) {
-            return bt(r + 1, 0);
-        }
-        if (b[r][c] == '.') {
-            return bt(r, c + 1);
-        }
-        int d = b[r][c] - '0';
-        rb[r][d] = true;
-        cb[c][d] = true;
-        bb[(r / 3) * 3 + (c / 3)][d] = true;
-        return bt(r, c + 1);
     }
 
-    bool ss(int r, int c) {
-        if (r == 9) {
+    bool solve(int r, int c) {
+
+        // board solved
+        if(r == 9)
             return true;
-        }
-        if (c == 9) {
-            return ss(r + 1, 0);
-        }
-        if (b[r][c] != '.') {
-            return ss(r, c + 1);
-        }
-        for (char i = '1'; i <= '9'; i++) {
-            int d = i - '0';
-            if (rb[r][d] || cb[c][d] || bb[(r / 3) * 3 + (c / 3)][d]) {
+
+        // move to next row
+        if(c == 9)
+            return solve(r + 1, 0);
+
+        // already filled cell
+        if(b[r][c] != '.')
+            return solve(r, c + 1);
+
+        int box = (r/3)*3 + (c/3);
+
+        // try digits 1-9
+        for(char ch = '1'; ch <= '9'; ch++) {
+
+            int d = ch - '0';
+
+            // unsafe
+            if(rb[r][d] || cb[c][d] || bb[box][d])
                 continue;
-            }
-            b[r][c] = i;
+
+            // place digit
+            b[r][c] = ch;
+
             rb[r][d] = true;
             cb[c][d] = true;
-            bb[(r / 3) * 3 + (c / 3)][d] = true;
-            if (ss(r, c + 1)) {
+            bb[box][d] = true;
+
+            // recurse
+            if(solve(r, c + 1))
                 return true;
-            }
+
+            // backtrack
             b[r][c] = '.';
+
             rb[r][d] = false;
             cb[c][d] = false;
-            bb[(r / 3) * 3 + (c / 3)][d] = false;
+            bb[box][d] = false;
         }
+
+        // no digit works
         return false;
     }
 
     void solveSudoku(vector<vector<char>>& board) {
+
         b = board;
+
         rb = vector<vector<bool>>(9, vector<bool>(10, false));
+
         cb = vector<vector<bool>>(9, vector<bool>(10, false));
+
         bb = vector<vector<bool>>(9, vector<bool>(10, false));
-        bt(0, 0);
-        ss(0, 0);
+
+        preprocess();
+
+        solve(0, 0);
+
         board = b;
     }
 };
